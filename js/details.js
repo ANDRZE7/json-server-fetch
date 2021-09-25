f721f172fafcb345c075cf758d899d668bac1b16
ANDRZE7 <andrzej.gagola@gmail.com>
2021-09-25 10:49:13 +0200

    Implemented blog deletion.

--- js/details.js
+++ js/details.js
@@ -6,8 +6,12 @@ const id = new URLSearchParams(window.location.search).get('id');
 // the container (html elem) to oudput the post's details
 const container = document.querySelector('.details');
 
+const deleteBtn = document.querySelector('.delete');
+
+// the uri to which the post and delete requests are sent to
+const uri = `http://localhost:3000/posts/${id}`;
+
 const renderDetails = async () => {
-  const uri = `http://localhost:3000/posts/${id}`;
   const res = await fetch(uri);
   const post = await res.json();
   console.log(post);
@@ -22,3 +26,10 @@ const renderDetails = async () => {
 
 // wait until the DOM content is loaded, then fire renderDetails funtion
 window.addEventListener('DOMContentLoaded', () => renderDetails());
+
+deleteBtn.addEventListener('click', async (e) => {
+  const res = await fetch(uri, {
+    method: 'DELETE',
+  });
+  window.location.replace('/index.html');
+});
