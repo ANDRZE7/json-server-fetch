78e8113f4a39a57569952fcc36c10bd3fd88e2c6
ANDRZE7 <andrzej.gagola@gmail.com>
2021-09-25 10:26:36 +0200

    Implemented adding a new blog item.

--- js/create.js
+++ js/create.js
@@ -1,1 +1,25 @@
 // javascript for create.html
+
+const form = document.querySelector('form');
+
+const createPost = async (e) => {
+  e.preventDefault(); // prevent the default action which is page reload
+
+  // prepare the json object of the blog's post to be posted to server
+  const doc = {
+    // don't add id for json server creats the id
+    title: form.title.value,
+    body: form.body.value,
+    likes: 0,
+  };
+
+  await fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    body: JSON.stringify(doc),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  window.location.replace('/index.html');
+};
+
+form.addEventListener('submit', createPost);
