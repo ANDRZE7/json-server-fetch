1f24d19ac931eb42613b74521564c6c4ae7e1d41
ANDRZE7 <andrzej.gagola@gmail.com>
2021-09-25 09:17:35 +0200

    Fetching all posts.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // javascript for index.html
+
+// the container where to output posts
+const container = document.querySelector('.blogs');
+
+const renderPosts = async () => {
+  let uri = 'http://localhost:3000/posts';
+
+  const res = await fetch(uri); // the res object is not a json, see next line to get json data from response
+  const posts = await res.json();
+  console.log(posts);
+
+  // prepare the html to display post by iterating json posts
+  let template = '';
+  posts.forEach((post) => {
+    template += `
+      <div class="post">
+        <h2>${post.title}</h2>
+        <p><small>${post.likes} like(s)</small></p>
+        <p>${post.body.slice(0, 200)}</p>
+        <a href="details.html">read more...<a/>
+      </div>
+    `;
+  });
+
+  // inject the html template
+  container.innerHTML = template;
+};
+
+// After DOM Content is loaded fire a function to load posts.
+window.addEventListener('DOMContentLoaded', () => renderPosts());
